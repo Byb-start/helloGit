a7ffb597d95362a71c9c7c47d999e31840366a1b
Byb-start <393417597@qq.com>
2020-09-27 00:41:17 +0800

    第一次...

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("秋田的第一杯奶茶！");
+        System.out.println("真·狗粮");
     }
 }
